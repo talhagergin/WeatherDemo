a28ea47a9fbb6a6189f159defdd0d6ffc7df8b29
Talha Gergin <talhagergin6@gmail.com>
2024-07-04 21:53:54 +0300

    Initial Commit

---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Talha Gergin on 4.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
