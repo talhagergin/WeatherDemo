03aa8a4269c0ddc26edb293ef469251a8067a495
Talha Gergin <talhagergin6@gmail.com>
2024-07-15 14:57:48 +0300

    Last Version

---
+++ WeatherDemo/Components/WeatherRow.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherRow.swift
+//  WeatherDemo
+//
+//  Created by Talha Gergin on 15.07.2024.
+//
+
+import SwiftUI
+
+struct WeatherRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherRow()
+}
